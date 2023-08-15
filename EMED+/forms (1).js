16a76c13f8bc32c7ed5dd0c48ae79666ff810fd5
Rafael Cerazi Fabriga <134517144+Azwaza@users.noreply.github.com>
2023-08-15 16:03:42 -0300

    Add files via upload

---
+++ EMED+/forms (1).js
@@ -0,0 +1,13 @@
+    var dado1 = document.getElementById('dado1');
+    var dado2 = document.getElementById('dado2');
+    var btn = document.getElementById('btn')
+    var resultado = document.getElementById('resultado')
+
+    btn.addEventListener('click', function(){
+        somatotal();
+    });
+
+    function somatotal(){
+        var soma2val = parseInt(dado1.value);
+        var soma2val = parseInt(dado2.value);
+    }
